24572748167b39b793865e774177c031bf9c89c3
Mennah Elsheikh <124211281+Mennah-Elsheikh@users.noreply.github.com>
2024-06-03 03:27:09 +0300

    Time: 18 ms (67.70%) | Memory: 11.7 MB (99.48%) - LeetSync

---
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int p1 = 0, p2 = 0;
+        while (p1 < s.size() && p2 < t.size()) {
+            if (s[p1] == t[p2]) {
+                p2++;
+            }
+            p1++;
+        }
+        return t.size() - p2;
+    }
+};
