17b11e9682ca85aeb9a496a3937d1b96d48269ee
Mennah Elsheikh <124211281+Mennah-Elsheikh@users.noreply.github.com>
2024-06-11 19:41:50 +0300

    Time: 0 ms (100.00%) | Memory: 9.5 MB (80.92%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,16 +1,27 @@
-class Solution {
+ class Solution {
 public:
-    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        int count = 0; 
-        for (int i = 0; i < arr2.size(); ++i) {
-            for (int j = 0; j < arr1.size(); ++j) {
-                if (arr1[j] == arr2[i]) {
-                    swap(arr1[j], arr1[count]);
-                    count++;
-                }
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2)
+     {
+        vector<int>freq(1001);
+        vector<int>arr;
+        for(int i = 0; i < arr1.size(); i++)
+        {
+            freq[arr1[i]]++;
+        }
+        for(int i = 0; i < arr2.size(); i++)
+        {
+            while(freq[arr2[i]]--)
+            {
+                arr.push_back(arr2[i]);
             }
         }
-        sort(arr1.begin() + count, arr1.end());  
-        return arr1;
+        sort(arr1.begin(),arr1.end());
+
+        for(int i = 0; i < arr1.size(); i++)
+        {
+            if(freq[arr1[i]] > 0)
+             arr.push_back(arr1[i]);
+        }
+        return arr;
     }
 };
