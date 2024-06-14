22ce8b40ea7e58fdd6d62b03eee8d1edcbfb3884
Mennah Elsheikh <124211281+Mennah-Elsheikh@users.noreply.github.com>
2024-06-14 20:33:26 +0300

    Time: 118 ms (65.94%) | Memory: 69.2 MB (50.39%) - LeetSync

---
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        /*
+        [1 , 2 , 2] => [1 , 2 , 3]
+        [3 , 2 , 1 , 2 , 1 , 7 ] => [ 3 , 4 , 1 , 2 , 5 , 7]
+        Mmmmmm [1 , 1 , 2 , 2 , 3 , 7 ] => [1 , 1+1 , 2+1 , 2+2 , 3+2 , 7]
+        */
+        int n  = nums.size();
+        int count = 0 ; 
+        sort(nums.begin() , nums.end());
+        for(int i = 0 ; i < n - 1 ;  i++)
+        {
+            int increment = 0 ; 
+            if(nums[i+1] <= nums[i])
+            {
+                increment = (nums[i] - nums[i+1]) + 1 ;
+                nums[i+1] += increment ;
+                count += increment ; 
+            }
+        }
+        return count ; 
+    }
+};
