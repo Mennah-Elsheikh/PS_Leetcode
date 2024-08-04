e62db0074bf0e81bab12a769cc38c35acfb6461f
Mennah Elsheikh <124211281+Mennah-Elsheikh@users.noreply.github.com>
2024-08-05 00:24:14 +0300

    Time: 25 ms (88.53%) | Memory: 26 MB (70.13%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int rangeSum(vector<int>& nums, int n, int left, int right) {
+    int rangeSum(std::vector<int>& nums, int n, int left, int right) {
         vector<int> sum;
         for (int i = 0; i < n; i++) {
             int currentSum = 0;
@@ -9,12 +9,14 @@ public:
                 sum.emplace_back(currentSum);
             }
         }
-        sort(sum.begin(), sum.end());
+        nth_element(sum.begin(), sum.begin() + left - 1, sum.end());
+        nth_element(sum.begin() + left - 1, sum.begin() + right, sum.end());
+        //sort(sum.begin() + left - 1, sum.begin() + right);
         int ans = 0;
         int MOD = 1000000007;
         for (int i = left - 1; i < right; i++) {
             ans = (ans + sum[i]) % MOD;
-        } 
+        }
         return ans;
     }
 };
