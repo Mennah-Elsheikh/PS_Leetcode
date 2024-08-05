585949b5c495979e0e1e8b8359b89923bd77fc30
Mennah Elsheikh <124211281+Mennah-Elsheikh@users.noreply.github.com>
2024-08-06 02:29:42 +0300

    Time: 11 ms (90.53%) | Memory: 18.5 MB (33.55%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
@@ -3,11 +3,13 @@ public:
     string kthDistinct(vector<string>& arr, int k) {
         cin.sync_with_stdio(0); cin.tie(0); cout.tie(0);
         unordered_map<string, int> mp;
-        for (auto i : arr) 
-            mp[i]++;
-        for (auto i : arr)
-            if (mp[i] == 1&&!(--k))
-                return i;
+        for(string x : arr) {
+            mp[x]++;
+        }
+        for(auto& s : arr) {
+            if(mp[s] == 1 && --k == 0)
+                return s;
+        }
         return "";
     }
 };
