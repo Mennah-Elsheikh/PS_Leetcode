cb2938947a327d9b6082b58227b83afc2b3c7c5c
Mennah Elsheikh <124211281+Mennah-Elsheikh@users.noreply.github.com>
2025-09-17 05:13:55 +0300

    Time: 867 ms (63.43%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
-select distinct author_id as id
-from Views
-where author_id = viewer_id
-order by id;
+select distinct author_id as id 
+from Views 
+where author_id = viewer_id 
+order by author_id asc;
