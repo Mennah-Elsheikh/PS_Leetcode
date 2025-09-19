48514c04cd3e7135fde5c52178395431d4771b1e
Mennah Elsheikh <124211281+Mennah-Elsheikh@users.noreply.github.com>
2025-09-19 18:49:45 +0300

    Time: 3015 ms (36.48%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,7 +1,5 @@
 /* Write your T-SQL query statement below */
-select distinct r.contest_id , round(count(distinct r.user_id) * 100.00/ (select count( distinct user_id) from Users ), 2 )  as percentage 
-from Register as r 
-join Users as u 
-on u.user_id = r.user_id 
-group by r.contest_id 
-order by percentage desc, r.contest_id asc ;  
+select distinct contest_id , round(count(*) * 100.00/ (select count( distinct user_id) from Users ), 2 )  as percentage 
+from Register 
+group by contest_id 
+order by percentage desc, contest_id asc ;  
